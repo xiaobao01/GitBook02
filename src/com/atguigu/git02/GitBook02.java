df6edf453e52e6850933b1cc81c0917d8806d414
khl775826762 <265904588@qq.com>
2016-12-29 16:34:03 +0800

    upate01

--- src/com/atguigu/git02/GitBook02.java
+++ src/com/atguigu/git02/GitBook02.java
@@ -5,6 +5,10 @@ public class GitBook02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("图书book02 ---->:xiaobao01");
+        
+        //khl第一次修改项目 KHL Update01
+        
+       
 	}
 
 }
